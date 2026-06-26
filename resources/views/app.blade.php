ccb9fa30cd3b2c735a449a925f346d718ddcd730
zainurroziqin <zainurroziqin38@gmail.com>
2026-06-26 14:43:10 +0700

    fix: change PWA icon purpose to 'any' to allow transparent background without black box fallback

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,7 @@
 
         <link rel="icon" href="/images/logo_golkar.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/images/logo_golkar.svg">
-        <link rel="manifest" href="/manifest.json?v=2">
+         <link rel="manifest" href="/manifest.json?v=3">
         <meta name="theme-color" content="#111827">
 
         <!-- Open Graph / Facebook / WhatsApp Sharing Meta Tags -->
